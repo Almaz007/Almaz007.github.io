79dcbb36ef90f0ede3dae9f078a5f8882ada2b21
Almaz007 <almazkhv1@gmail.com>
2025-09-11 09:28:49 +0300

    m

---
+++ assets/store-G0C_JJSk.js
@@ -0,0 +1,1 @@
+import{c as i}from"./shallow-DV_ax3xe.js";const c=i((s,n)=>({config:{scripts:[],functions:[],setpoints:[],outputMatrix:[],inputMatrix:[],sms:"",smsWeb:"",phoneNumber:"",offsetsOsc:[]},setConfig(o){s(t=>({...t,config:{...t.config,...o}}))}}));export{c as u};
